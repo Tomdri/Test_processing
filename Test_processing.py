c1c112db9b12c74eb7630ba1ad7d5c47ed781021
Tomdri <thomas.dufour@polymtl.ca>
2021-02-11 10:54:07 -0500

    Ajout bonne methode

--- Test_processing.py
+++ Test_processing.py
@@ -1,49 +1,45 @@
 import cv2 as cv
 import numpy as np
+import PIL
+import glob
 from skimage import io
 from matplotlib import pyplot as plt
-import PIL 
 from PIL import Image
-import glob
 
-def noiseReduction(img, i):
-    dst = cv.fastNlMeansDenoisingColored(img,None,10,10,7,21)
-    cv.imwrite(r"C:/Users/tomsw/Documents/Session/Projy/New_images_noise/new_tom"+str(i)+".jpg", dst)
-    
-    # Afficher l'image
-    # plt.subplot(121),plt.imshow(img)
-    # plt.subplot(122),plt.imshow(dst)
-    # plt.show()
+def findContour(img, j):
+    cimg = img.copy()
+    finalimg = img.copy()
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.medianBlur(img, 5)
+    circles = cv.HoughCircles(image=img, method=cv.HOUGH_GRADIENT_ALT, dp=0.9, 
+                            minDist=1, param1=40, param2=0.1, minRadius= 100, maxRadius=175)
 
-def findCountour(img, i):
-    gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-    _, binary = cv.threshold(gray, 90, 255, cv.THRESH_BINARY_INV)
-    contours, hierarchy = cv.findContours(binary, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            centerX = i[0]
+            centerY = i[1]
+            radius = i[2]
+            # Draw circle around tomato
+            cv.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+            # Draw center of tomato
+            cv.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+            # cropTomato(finalimg, centerX, centerY, radius, j)
 
-    if len(contours) != 0:
-        for i in range(len(contours)):
-            # cv.drawContours(thresh,findLargestContour(contours),-1,(150,10,255),3)
-            if len(contours[i]) >= 5:
-                img = cv.drawContours(img,contours,-1,(150,10,255),3)
-                # ellipse=cv.fitEllipse(contours[i])
-            # else:
-            #     img = cv.drawContours(img,contours,-1,(0,0,0),-1)
+    cv.imwrite(r"C:/Users/tomsw/Documents/Session/Test_processing/Processed_tom/new_tom"+str(j)+".jpg", cimg)
 
-    cv.imwrite(r"C:/Users/tomsw/Documents/Session/Projy/New_images_cont/new_tom"+str(i)+".jpg", img)
+def cropTomato(img, x, y, r, j):
+    crop_img = img[y-r:y+r, x-r:x+r]
+    cv.imwrite(r"C:/Users/tomsw/Documents/Session/Test_processing/Processed_tom/new_tom"+str(j)+".jpg", crop_img)
 
-def findLargestContour(contours):
-    areas = [cv.contourArea(c) for c in contours]
-    max_index = np.argmax(areas)
-    return contours[max_index]
 
 def main():
-    i = 0
-    images = [cv.imread(file) for file in glob.glob(r"C:/Users/tomsw/Documents/Session/Projy/Tomates/tomate_12/*.jpg")]
+    j = 0
+    images = [cv.imread(file) for file in glob.glob(r"C:/Users/tomsw/Documents/Session/Test_processing/Tomates/tomate_12/*.jpg")]
     for image in images:
-        noiseReduction(image, i)
-        findCountour(image, i)
-        i+=1
-        break
+        findContour(image, j)
+        j+=1
+        # break
 
 if __name__ == "__main__":
     main()
